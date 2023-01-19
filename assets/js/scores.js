2ec51fa10f0f5597d7981923d89b430c3f9cb0cb
Martin Graham <martin.graham.web@gmail.com>
2023-01-19 19:03:12 +0000

    added display and append to allow listing scores

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,3 +1,17 @@
 // Get the initials and score from the local storage
 let initials = localStorage.getItem("initials");
 let score = localStorage.getItem("score");
+
+// Display the initials and score on the page
+if (initials !== null && score !== null) {
+    let highscoresList = document.getElementById("highscores");
+    let highscoreItem = document.createElement("li");
+    let initialsSpan = document.createElement("span");
+    initialsSpan.innerHTML = initials;
+    let scoreSpan = document.createElement("span");
+    scoreSpan.innerHTML = score;
+  
+    highscoreItem.appendChild(initialsSpan);
+    highscoreItem.appendChild(scoreSpan);
+    highscoresList.appendChild(highscoreItem);
+  }
